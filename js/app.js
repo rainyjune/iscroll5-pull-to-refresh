2752dd9ac5d9d1a327d4bc6165c3adcb7a5e3190
rainyjune <rainyjune@live.cn>
2015-07-21 10:54:59 +0800

    Move JavaScript code into the js/app.js file

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var myScroll;
+
+function loaded () {
+	myScroll = new IScroll('#wrapper', { mouseWheel: true });
+}
+
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
