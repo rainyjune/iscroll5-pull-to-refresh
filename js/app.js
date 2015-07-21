4d4b86901efbfc976ff2daefef61d14934dd3f19
rainyjune <rainyjune@live.cn>
2015-07-21 11:19:32 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 var myScroll;
 
+var pullDownEl, pullDownElHeight,
+    pullUpEl, pullUpElHeight;
+
 function loaded () {
-	myScroll = new IScroll('#wrapper', { mouseWheel: true });
+	myScroll = new IScroll('#wrapper', { 
+    probeType: 3,
+    mouseWheel: true });
+
+  pullDownEl = document.getElementById("pullDown");
+  pullDownElHeight = pullDownEl.offsetHeight;
+  pullUpEl = document.getElementById("pullUp");
+  pullUpElHeight = pullUpEl.offsetHeight;
+
+  myScroll.on("scroll", logPosition);
+  myScroll.on("scrollEnd", logPosition);
 }
 
 document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+function logPosition() {
+  console.log("Position:", parseInt(myScroll.y));
+}
