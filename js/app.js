eb5aa16c081be574cc27574fe056b782585d3ce0
rainyjune <rainyjune@live.cn>
2015-07-21 14:08:18 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
-var myScroll;
+var myScroll,
+    maxScrollY;// local variable
 
-var pullDownEl, pullDownElHeight,
-    pullUpEl, pullUpElHeight;
+var pullDownEl, pullDownLabel, pullDownElHeight,
+    pullUpEl, pullUpLabel, pullUpElHeight;
 
 function loaded () {
 	myScroll = new IScroll('#wrapper', { 
     probeType: 3,
+    scrollbars: true,
     mouseWheel: true });
 
   pullDownEl = document.getElementById("pullDown");
@@ -13,11 +15,72 @@ function loaded () {
   pullUpEl = document.getElementById("pullUp");
   pullUpElHeight = pullUpEl.offsetHeight;
 
-  myScroll.on("scroll", logPosition);
-  myScroll.on("scrollEnd", logPosition);
+  pullDownLabel = pullDownEl.querySelector(".pullDownLabel");
+  pullUpLabel = pullUpEl.querySelector(".pullUpLabel");
+
+  maxScrollY = myScroll.maxScrollY; 
+
+  myScroll.on("scroll", onScrollMove);
+  myScroll.on("scrollEnd", onScrollEnd);
+}
+
+function onScrollMove() {
+  logPosition();
+  var y = this.y;
+  if (pullDownEl && pullDownElHeight && y >= pullDownElHeight && !pullDownEl.className.match("flip")) {
+    pullDownAction();
+  } else if (pullDownEl && pullDownElHeight && y < pullDownElHeight && pullDownEl.className.match("flip")) {
+    restorePullDownButton();
+  } else if (pullUpEl && pullUpElHeight && (y <= maxScrollY - pullUpElHeight) && !pullUpEl.className.match("flip")) {
+    pullUpAction();
+  } else if (pullUpEl && pullUpElHeight && (y > maxScrollY - pullUpElHeight) && pullUpEl.className.match("flip")) {
+    restorePullUpButton();
+  }
+}
+
+function onScrollEnd() {
+  var flipEl;
+  if (pullDownEl.className.match("flip")) {
+    flipEl = pullDownEl;
+  } else if (pullUpEl.className.match("flip")) {
+    flipEl = pullUpEl;
+  }
+  if (flipEl) {
+    setLoadingStatus(flipEl);
+  }
 }
 
 document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+function setLoadingStatus(flipEl) {
+  flipEl.className = "loading";
+  var label = flipEl.querySelector('span[class$="Label"]');
+  label.innerHTML = label.getAttribute("data-loadingText");
+}
+
+function pullDownAction() {
+  console.log("Pull down");
+  pullDownEl.className = "flip";
+  pullDownLabel.innerText = pullDownLabel.getAttribute("data-flipText"); 
+}
+
+function restorePullDownButton() {
+  pullDownEl.className = "";
+  pullDownLabel.innerText = pullDownLabel.getAttribute("data-defaultText"); 
+}
+
+function pullUpAction() {
+  console.log("Pull up");
+  pullUpEl.className = "flip";
+  pullUpLabel.innerText = pullUpLabel.getAttribute("data-flipText");
+  myScroll.maxScrollY = maxScrollY - pullUpElHeight;
+}
+
+function restorePullUpButton() {
+  pullUpEl.className = "";
+  pullUpLabel.innerText = pullUpLabel.getAttribute("data-defaultText"); 
+  myScroll.maxScrollY = maxScrollY; 
+}
 
 function logPosition() {
   console.log("Position:", parseInt(myScroll.y));
